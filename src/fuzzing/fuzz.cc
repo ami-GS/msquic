fe8c6c2ec7b5cf6e9f7379dbe1449c3cacad16e3
ami-GS <1991.daiki@gmail.com>
2022-10-07 09:24:16 -0700

    tmp

--- src/fuzzing/fuzz.cc
+++ src/fuzzing/fuzz.cc
@@ -9,21 +9,23 @@ Abstract:
 
 --*/
 
-#define CX_PLATFORM_LINUX 1
-#define QUIC_TEST_APIS 1
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <string>
 #include "msquic.h"
 #include "msquic.hpp"
 #include "quic_platform.h"
+#include <iostream>
 
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
 	const MsQuicApi* MsQuic = new(std::nothrow) MsQuicApi();
-
+	std::cout << "[";
+	for (size_t i = 0; i < size; i++) {
+		std::cout << data[i];
+	}
+	std::cout << std::endl;
 	for (uint32_t Param = QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT;
 		Param <= QUIC_PARAM_GLOBAL_TLS_PROVIDER;
 		Param++) {
