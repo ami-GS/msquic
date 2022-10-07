52d7921839bba26731b6fc3a550d592f480f3185
ami-GS <1991.daiki@gmail.com>
2022-10-07 14:45:15 -0700

    back

--- src/fuzzing/fuzz.cc
+++ src/fuzzing/fuzz.cc
@@ -33,7 +33,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 	for (uint32_t Param = QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT;
 		Param <= QUIC_PARAM_GLOBAL_TLS_PROVIDER;
 		Param++) {
-			if (Param == QUIC_PARAM_GLOBAL_VERSION_SETTINGS)
+			if (Param == 0x01000007)
 				continue;
 		// std::cerr << Param - QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT << ":";
 		auto out = MsQuic->SetParam(
