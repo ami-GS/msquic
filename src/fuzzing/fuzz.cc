68576843c0d569d1fe118835214e1e76143f0b9f
ami-GS <1991.daiki@gmail.com>
2022-10-07 09:39:25 -0700

    detail

--- src/fuzzing/fuzz.cc
+++ src/fuzzing/fuzz.cc
@@ -16,25 +16,27 @@ Abstract:
 #include "msquic.hpp"
 #include "quic_platform.h"
 #include <iostream>
-
+#include <bitset>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
 	const MsQuicApi* MsQuic = new(std::nothrow) MsQuicApi();
-	std::cout << "[";
+	std::cout << "size:" << size << "\t[";
 	for (size_t i = 0; i < size; i++) {
-		std::cout << std::hex << data[i] << " ";
+		std::cout << std::bitset<8>{data[i]} << " ";
 	}
-	std::cout << std::dec << "]:" << size << std::endl;
+	std::cout << std::dec << "]" << ", Param:";
 	for (uint32_t Param = QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT;
 		Param <= QUIC_PARAM_GLOBAL_TLS_PROVIDER;
 		Param++) {
+		std::cout << Param;
 		MsQuic->SetParam(
 			nullptr,
 			Param,
 			size,
 			&data);
 	}
+	std::cout << std::endl;
 
 	delete MsQuic;
 	return 0;
