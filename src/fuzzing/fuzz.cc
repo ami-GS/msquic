98384b84476fb21d7dbe96c244471877dcb36fab
ami-GS <1991.daiki@gmail.com>
2022-10-07 11:49:40 -0700

    maybe fix...?

--- src/fuzzing/fuzz.cc
+++ src/fuzzing/fuzz.cc
@@ -40,7 +40,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 			Param,
 			size,
 			//&data);
-			&dd);
+			dd);
 		std::cerr << out << ",";
 	}
 	std::cerr << std::endl;
